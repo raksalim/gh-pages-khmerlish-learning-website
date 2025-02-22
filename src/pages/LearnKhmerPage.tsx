d9e9cc231ac089966ce81bb83cb7f7bc529a15bd
Raksa Lim <raksalim97@gmail.com>
2025-02-22 00:23:26 -0500

    removed React import

--- src/pages/LearnKhmerPage.tsx
+++ src/pages/LearnKhmerPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import LearnKhmerCard from '../components/LearnKhmerCard'
 import { khmerVideosJson } from '../data/data'
 
