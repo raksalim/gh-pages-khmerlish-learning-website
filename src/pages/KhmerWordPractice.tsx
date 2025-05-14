d2f2745cdd76eb3d75ba981fdc93200199bd2c57
Raksa Lim <raksalim97@gmail.com>
2025-05-13 22:27:14 -0400

    add spelling logic

--- src/pages/KhmerWordPractice.tsx
+++ src/pages/KhmerWordPractice.tsx
@@ -10,11 +10,20 @@ import { khmerVowels, khmerVowelsThatDontChange } from "../data/khmer/khmerVowel
 export const KhmerWordPractice = () => {
     // const [isColorEnabled, setIsColorEnabled] = useState(true);
     const [consonent, setConsonent] = useState<string>('ក')
+    const [jung, setJung] = useState<string>('')
+    const [isConsonent, setIsConsonent] = useState<boolean>(true)
     const [vowel, setVowel] = useState<string>('ា')
     const [isAh, setIsAh] = useState<boolean>(true)
 
     const setVowelColor = () =>
         isAh ? "primary" : "error"
+
+    const setDisplayLetter = (letter: string) => {
+        if (isConsonent) {
+            return (letter === 'ឡ') ? letter : letter + "្" + letter
+        }
+        return "្" + letter
+    }
     const formatIdx = (idx: number) => idx <= 9 ? `0${idx}` : `${idx}`;
 
 
@@ -45,7 +54,7 @@ export const KhmerWordPractice = () => {
             </style>
             <div className="word-container"
                 style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100px', fontSize: '25px', margin: 'auto' }}>
-                {`${consonent}  +  ${vowel} = ${consonent + vowel}`}
+                {`${jung === '' ? consonent : consonent + jung}  +  ${vowel} = ${consonent + vowel}`}
             </div>
 
             <div className="flex-container" style={{ display: "flex", justifyContent: "center", alignItems: "center", flexWrap: "wrap" }}>
@@ -60,16 +69,39 @@ export const KhmerWordPractice = () => {
                                     fullWidth
                                     style={{ fontSize: "20px" }}
                                     onClick={() => {
-                                        setConsonent(khmerConsonant);
-                                        setIsAh(khmerConsonantsAh.includes(khmerConsonant));
+                                        if (!isConsonent) {
+                                            setJung("្" + khmerConsonant);
+                                            setIsConsonent(true);
+                                        }
+                                        if (jung !== '') {
+                                            setJung('');
+                                        }
+                                        if (isConsonent) {
+                                            setConsonent(khmerConsonant);
+                                            setIsAh(khmerConsonantsAh.includes(khmerConsonant));
+                                        }
                                         new Audio(`/sound/khmer/consonants/c-${formatIdx(idx + 1)}.wav`).play()
                                     }}
                                 >
-                                    {(khmerConsonant === 'ឡ') ? khmerConsonant : khmerConsonant + "្" + khmerConsonant}
+                                    {setDisplayLetter(khmerConsonant)}
                                 </Button>
                             </div>
                         )
                     }
+                    <Button
+                        variant='outlined'
+                        color='primary'
+                        fullWidth
+                        style={{ fontSize: "10px", marginLeft: "auto", gridColumn: "span 2" }}
+                        onClick={() => {
+                            setIsConsonent(!isConsonent)
+                        }}
+                    >
+                        <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                            <span style={{ border: "2px dotted", borderRadius: "50%", padding: "5px" }}></span>
+                            <span>+</span>
+                        </div>
+                    </Button>
                 </div>
                 <div style={{ width: '100px', height: '50px' }} />
                 <div style={{ display: "grid", gridTemplateColumns: "repeat(5, 1fr)", gap: "7px", maxWidth: "500px", marginBottom: "auto" }}>
@@ -84,8 +116,8 @@ export const KhmerWordPractice = () => {
                                     onClick={() => {
                                         setVowel(vowel);
                                         const soundPath = (isAh || khmerVowelsThatDontChange.includes(vowel))
-                                            ? `/sound/khmer/vowles-set-1/v1-${formatIdx(idx + 1)}.wav`
-                                            : `/sound/khmer/vowles-set-2/v2-${formatIdx(idx + 1)}.wav`
+                                            ? `/sound/khmer/vowels-set-1/v1-${formatIdx(idx + 1)}.wav`
+                                            : `/sound/khmer/vowels-set-2/v2-${formatIdx(idx + 1)}.wav`
                                         new Audio(soundPath).play()
                                     }}
                                 >
@@ -94,7 +126,20 @@ export const KhmerWordPractice = () => {
                             </div>
                         )
                     }
+                    <Button
+                        variant='outlined'
+                        color='primary'
+                        fullWidth
+                        style={{ fontSize: "15px", marginLeft: "auto", gridColumn: "span 2" }}
+                        onClick={() => {
+                            setConsonent(!isAh ? 'ក' : 'គ')
+                            setIsAh(!isAh)
+                        }}
+                    >
+                        {"<=>"}
+                    </Button>
                 </div>
+
             </div>
 
             <div style={{ textAlign: "center", fontSize: "14px", paddingTop: "30px" }}>
@@ -104,7 +149,7 @@ export const KhmerWordPractice = () => {
                     <VolumeUpRoundedIcon style={{ fontSize: "30px" }} />
                 </div>
 
-                <p>Khmer Vowels Set 2</p>
+                <p>Khmer Independent Vowels</p>
                 <p>ABC's Keyboard</p>
                 <p>Coming Soon</p>
             </div>
