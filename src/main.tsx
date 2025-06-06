4c815430c4a8edeb343e96c1d936db4675b3d8b6
Raksa Lim <raksalim97@gmail.com>
2025-06-05 23:16:16 -0400

    secret name

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,9 @@ import { createRoot } from 'react-dom/client'
 // import './index.css'
 import App from './App.tsx'
 
-
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_JhDadWAl8",
-  client_id: process.env.COGNITO_CLIENT_ID || '',
+  client_id: import.meta.env.VITE_COGNITO_USER_POOL_CLIENT_ID || '',
   redirect_uri: window.location.hostname.includes('localhost') ? "http://localhost:5173" : "https://www.khmerlish.net",
   response_type: "code",
   scope: "phone openid email",
